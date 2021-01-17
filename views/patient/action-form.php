c81150cdda6cade9e766ac8251e779615f9bf193
Rumeysa ÜSTÜN <59111328+rumeysaustun@users.noreply.github.com>
2021-01-17 03:08:51 +0300

    Update action-form.php

--- views/patient/action-form.php
+++ views/patient/action-form.php
@@ -1,2 +1,2 @@
 <h2><?= $patient->name . ' ' . $patient->family ?></h2>
-<?= \pistol88\hospital\widgets\ActionForm::widget(['pjax' => $pjax, 'model' => $model, 'patient' => $patient]); ?>
+<?= \rumeysaustun\hospital\widgets\ActionForm::widget(['pjax' => $pjax, 'model' => $model, 'patient' => $patient]); ?>
