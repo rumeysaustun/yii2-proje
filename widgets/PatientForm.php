1a78ba4880392b193e1595471e2db1e43a126c3f
Rumeysa ÜSTÜN <59111328+rumeysaustun@users.noreply.github.com>
2021-01-17 03:13:41 +0300

    Update PatientForm.php

--- widgets/PatientForm.php
+++ widgets/PatientForm.php
@@ -1,9 +1,9 @@
 <?php
-namespace pistol88\hospital\widgets;
+namespace rumeysaustun\hospital\widgets;
 
 use yii\base\Widget;
-use pistol88\hospital\models\PatientForm as Model;
-use pistol88\hospital\traits\Module;
+use rumeysaustun\hospital\models\PatientForm as Model;
+use rumeysaustun\hospital\traits\Module;
 
 class PatientForm extends Widget
 {
