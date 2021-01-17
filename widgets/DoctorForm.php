df593a12741ec98d2188fd04bf3d63c36c896794
Rumeysa ÜSTÜN <59111328+rumeysaustun@users.noreply.github.com>
2021-01-17 03:12:57 +0300

    Update DoctorForm.php

--- widgets/DoctorForm.php
+++ widgets/DoctorForm.php
@@ -1,9 +1,9 @@
 <?php
-namespace pistol88\hospital\widgets;
+namespace rumeysaustun\hospital\widgets;
 
 use yii\base\Widget;
-use pistol88\hospital\models\DoctorForm as Model;
-use pistol88\hospital\traits\Module;
+use rumeysaustun\hospital\models\DoctorForm as Model;
+use rumeysaustun\hospital\traits\Module;
 
 class DoctorForm extends Widget
 {
