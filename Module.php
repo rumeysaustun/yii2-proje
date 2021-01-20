5d0517fc60286ef9603eeb172d7fe1f873a6ae54
Rumeysa ÜSTÜN <59111328+rumeysaustun@users.noreply.github.com>
2021-01-20 22:00:47 +0300

    Update Module.php

--- Module.php
+++ Module.php
@@ -5,4 +5,23 @@ class Module extends \yii\base\Module
 {
     public $captcha = false; //show captcha
     public $adminRoles = ['@']; //['superadmin', 'administrator', 'admin']; //roles who can see orders
+
+    public function init()
+    {
+        parent::init();
+
+        $this->registerTranslation();
+    }
+
+    public function registerTranslation()
+    {
+        if (!isset(\Yii::$app->i18n->translations['hospital'])) {
+            \Yii::$app->i18n->translations['hospital'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'basePath' => __DIR__.'/messages',
+                'forceTranslation' => true
+            ];
+        }
+    }
 }
+
